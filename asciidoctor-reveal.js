a1ff2222c2c5105b8ddde369d04c5283b953afc9
binout <benoit.prioux@gmail.com>
2023-03-02 21:05:51 +0100

    add slide template

---
+++ asciidoctor-reveal.js
@@ -0,0 +1,8 @@
+// Load Asciidoctor.js and the reveal.js converter
+const asciidoctor = require('@asciidoctor/core')()
+const asciidoctorRevealjs = require('@asciidoctor/reveal.js')
+asciidoctorRevealjs.register()
+
+// Convert the document 'presentation.adoc' using the reveal.js converter
+const options = { safe: 'safe', backend: 'revealjs' }
+asciidoctor.convertFile('presentation.adoc', {attributes: {conf : 'devoxx'}, ...options})
